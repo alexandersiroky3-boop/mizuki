55894a98bd2d8f3258026c0aafa573611586c2fe
alexandersiroky3-boop <alexandersiroky3@gmail.com>
2026-08-09 11:28:56 +0200

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -674,7 +674,9 @@ return message.channel.send(
 ${dialogue}
 
 💋 **${message.author.username} kissed ${target.username}!**
+
 💖 **${target.username} received +${reward.toLocaleString()} XP!**
+
 💕 **${message.author.username} received +${kisserReward.toLocaleString()} XP!**${usedLuckExtra}${luckExtra}`
 
 );
