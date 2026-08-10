ef58a552dfc3b6b94dc2e86a912acd3ac49dbb61
alexandersiroky3-boop <alexandersiroky3@gmail.com>
2026-08-10 12:13:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -735,7 +735,7 @@ if(result.critical){
 
         message.reply(
 
-            `💥 **${message.author.username} got ${result.criticalStreak} critical streaks!**\n🎯 Next critical chance: **${result.nextCriticalChance}%**`
+            `💥 **${message.author.username} got ${result.criticalStreak} critical streaks!**`
 
         ).catch(() => {});
 
@@ -764,7 +764,7 @@ else if(result.lostCriticalStreak >= 2){
 
     message.reply(
 
-        `💔 **${message.author.username} lost their ${result.lostCriticalStreak}x critical streak!`
+        `💔 **${message.author.username} lost their ${result.lostCriticalStreak}x critical streak!**`
 
     ).catch(() => {});
 
